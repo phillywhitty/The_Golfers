45c3ea4afe17ddf3a2acc0f970b7bfc27816f22b
phillywhitty <philipwhitty92@yahoo.ie>
2023-10-02 08:34:35 +0000

    add summernote

--- the_golfers/settings.py
+++ the_golfers/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-phillywhitty-thegolfers-32sxaj4omfe.ws-eu105.gitpod.io','localhost']
+ALLOWED_HOSTS = ['8000-phillywhitty-thegolfers-32sxaj4omfe.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog'
 ]
 
@@ -81,12 +82,12 @@ WSGI_APPLICATION = 'the_golfers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
