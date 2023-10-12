c59f2089b77b239a150d9ad10fc3d90c8fac7138
phillywhitty <philipwhitty92@yahoo.ie>
2023-10-12 10:26:15 +0000

    add url for home on nav bar to replicate landing_page

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
      path('', views.landing_page, name='landing_page'),
+     path('home', views.landing_page, name='home'),
      path("about/", views.about, name="about"),
      path("post_list/", views.PostList.as_view(), name="index"),
      path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
