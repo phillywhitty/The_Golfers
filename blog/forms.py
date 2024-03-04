d0c8d71b905c19c4608248ff9193dd91820c222e
phillywhitty <philipwhitty92@yahoo.ie>
2024-03-04 18:40:03 +0000

    rename a field in model and change background color for card body in html pages

--- blog/forms.py
+++ blog/forms.py
@@ -10,7 +10,7 @@ class CreateGolfBlogForm(ModelForm):
 
     class Meta:
         model = AddGolfCourse
-        fields = ['course_name', 'location', 'content', ]
+        fields = ['course_name', 'location', 'description', ]
         exclude = ['user', ]
 
 
