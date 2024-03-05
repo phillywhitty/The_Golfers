ead3fd520f245676d37342162c28134ebd7e68c0
phillywhitty <philipwhitty92@yahoo.ie>
2024-03-05 11:29:08 +0000

    set debug to false

--- the_golfers/settings.py
+++ the_golfers/settings.py
@@ -19,7 +19,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-golfers-blog-ed907c4b0918.herokuapp.com', 
                 '8000-phillywhitty-thegolfers-ohlgcowv5ox.ws-eu108.gitpod.io'
